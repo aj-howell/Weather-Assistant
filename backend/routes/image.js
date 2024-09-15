053ffb79068ef41ad61ada0e44b2d7695612fede
Sierra Obi <obi.sierra@gmail.com>
2024-09-15 12:26:42 -0400

    Revert "Backend set up"

--- backend/routes/image.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var imageController = require('../controllers/imageController.js');
-
-router.post('/', imageController.generatePictureDescription)
-
-module.exports = router;
